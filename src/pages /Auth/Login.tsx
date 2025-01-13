95d97e8ebc9b9acadfa19fa28b51ea0366e33a76
zin <rkfwoa02@naver.com>
2025-01-13 20:22:42 +0900

    회원가입 로그인 custom hook 분리

--- src/pages /Auth/Login.tsx
+++ src/pages /Auth/Login.tsx
@@ -1,38 +1,11 @@
 import AuthForm from '../../components/Auth/AuthForm.tsx';
-import { ChangeEvent, useState } from 'react';
-import { AuthFormType } from '../../types/common.ts';
-import { emailRegex, validateInput } from '../../utils/RegEx.ts';
 import { supabase } from '../../utils/SupabaseClient.ts';
 import { useNavigate } from 'react-router';
+import { useAuthInput } from '../../hooks/useAuthInput.tsx';
 
 export default function Login() {
   const navigate = useNavigate();
-  const [form, setForm] = useState<AuthFormType>({
-    email: '',
-    password: '',
-  });
-  const [error, setError] = useState<{ [key: string]: string }>();
-  const getAuthForm = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.name === 'email' && !emailRegex(e.target.value)) {
-      setError({ ...error, email: '이메일을 확인해주세요' });
-    } else {
-      const { email, ...restErrors } = error;
-      setError(restErrors);
-    }
-    if (e.target.name === 'password' && !validateInput(e.target.value, true)) {
-      setError({
-        ...error,
-        password: '특수문자, 숫자, 영문자 조합으로 입력해주세요.',
-      });
-    } else {
-      const { password, ...restErrors } = error;
-      setError(restErrors);
-    }
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  };
+  const { form, error, getAuthForm } = useAuthInput();
 
   const login = async () => {
     try {
